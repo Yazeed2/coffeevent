8ffdca0c3bea8aa19c8ff500b752c55c023a10a4
Ali-hd <ali.hamidaddin1@gmail.com>
2019-11-23 19:39:23 +0300

    added Event Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './components/HomePage'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer/Footer'
 import Search from './components/Search/Search'
+import EventPage from './components/EventPage/EventPage'
 
 export default class App extends Component {
   state = {
@@ -19,6 +20,9 @@ export default class App extends Component {
     return (
       <div>
         <Navigation />
+
+        {/* <EventPage/> */}
+
         <article className="main-intro">
           <Jumbotron fluid className="jumbotron-style">
             <Container className="jumbotron-content">
@@ -43,43 +47,7 @@ export default class App extends Component {
             <Button className="ShowHideButton" onClick={() => { this.setState({ arry: [1, 2, 3, 4], showAll: true, hideThem: false }) }}>Hide</Button>
           </div>
           : null}
-        {/* <Carousel >
-  <Carousel.Item style={{height:'50vh'}}>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/881TPou.png"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item style={{height:'50vh'}}>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/uQxAvwy.png"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item style={{height:'50vh'}}>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/Jw6nLQ9.png"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>    */}
+       
         <Footer />
       </div>
     )
