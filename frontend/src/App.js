1663751140963eff426284bf81589ba4e55a9cd7
Ali-hd <ali.hamidaddin1@gmail.com>
2019-11-26 19:36:51 +0300

    improving styling and responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,55 +35,17 @@ console.log(data1)
     return (
       <div>
 
+
 <BrowserRouter>
-   <Navbar bg="light" expand="sm">
-            <Navbar.Brand style={{ fontSize: '24px', fontWeight: 'bolder'}} href="#"><img style={{marginRight:'10px'}} alt="" src="https://i.imgur.com/eFCHtTt.png" width="30" height="30"/>COFFEVENT</Navbar.Brand>
-            
-    
-      <span className="AccountLabel">Account</span>
-
-    
-      
-            <NavDropdown drop={'down'}  
-                title={
-                       <FaUserCircle style={{marginLeft:'7px', marginRight:'7px',color: "black"}} size={30} />
-                    } 
-                id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">My Events</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>  
-            </NavDropdown>
-        
-
-        <FaMapMarkedAlt size={30} style={{marginLeft:'-5px', marginRight:'5px'}} />
-            
-                          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-
-                        <NavDropdown title="dropdown" id="basic-nav-dropdown" >
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                    </Nav>
-
-                </Navbar.Collapse> 
-            </Navbar>
-
+<Navigation/>
 
 <Switch>
-<Route path="/about" component={AllEvents}/>
-<Route path="/galery" component={Allshops}/>
+<Route path="/Events" component={AllEvents}/>
+<Route path="/Coffees" component={Allshops}/>
 <Route path="/Contact" component={CreateEvent}/>
 {/* <Route path="/Contact" component={Register}/> */}
 <Route path="/Contact" component={Signin}/>
-<Route path="/home" component={Home}/>
+<Route path="/Home" component={Home}/>
 
 
 
